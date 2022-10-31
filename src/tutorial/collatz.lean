fd712f8bdbffeb96c5d59af5c7d1364c6a0dbd1a
CBirkbeck <c.birkbeck@ucl.ac.uk>
2022-10-31 14:43:30 +0000

    up3

--- src/tutorial/collatz.lean
+++ src/tutorial/collatz.lean
@@ -6,6 +6,8 @@ import tactic --hide
 
 -/
 
+noncomputable theory --hide
+
 /-- The function which applies one step of the 3x+1 problem. -/
 def collatz (x : ℕ) := if x % 2 = 0 then x / 2 else 3 * x + 1
 
